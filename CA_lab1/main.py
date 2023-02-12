a18d0c7dbcf55bc1c3366d4b3046e73c92dd3a0f
nikitalystsev <96741101+nikitalystsev@users.noreply.github.com>
2023-02-12 17:01:08 +0300

    add files

--- CA_lab1/main.py
+++ CA_lab1/main.py
@@ -21,9 +21,18 @@ def main():
 
     diff = my_interp.get_corner(diff_table)
 
-    result = my_interp.newton_polynom(0.6, diff, points)
+    new_table = my_interp.get_points_table_for_hermite(points)
 
-    print(result)
+    diff_table_hermit = my_interp.get_diff_table_for_hermite(new_table)
+
+    my_print.print_matrix(diff_table_hermit)
+
+    diff_hermit = my_interp.get_corner(diff_table_hermit)
+
+    result = my_interp.newton_polynom(0.6, diff, points)
+    result2 = my_interp.newton_polynom(0.6, diff_hermit, new_table)
+
+    print("newton:", result, "hermit:", result2, sep='\n')
 
 
 if __name__ == "__main__":
--- CA_lab1/my_interpolation.py
+++ CA_lab1/my_interpolation.py
@@ -49,3 +49,42 @@ def newton_polynom(x, diff, points_table):
     return result
 
 
+def get_points_table_for_hermite(points_table):
+    """
+    Функция получает таблицу точек для полинома Эрмита
+    """
+    new_table = []
+
+    for point in points_table:
+        multiplicity = len(point) - 1  # кратность узла
+        for i in range(multiplicity):
+            new_table.append(point)
+
+    return new_table
+
+
+def get_diff_table_for_hermite(points_table):
+    """
+    Функция получает таблицу разделенных разностей для полинома Эрмита
+    """
+    count_points = len(points_table)
+
+    # создаем матрицу под разделенные разности
+    diff_table = [[0] * count_points for _ in range(count_points)]
+
+    # первый столбец заполняем значениями функций из таблицы в узлах
+    for i in range(count_points):
+        diff_table[i][0] = points_table[i][1]
+
+    # получаем разделенные разности
+    for i in range(1, count_points):
+        for j in range(i, count_points):  # обходим под главной диагональю
+            # j-i определяет диагональные элементы
+            if points_table[j][0] - points_table[j - i][0] != 0:
+                diff_table[j][i] = \
+                    (diff_table[j][i - 1] - diff_table[j - 1][i - 1]) / \
+                    (points_table[j][0] - points_table[j - i][0])
+            else:
+                diff_table[j][i] = points_table[j][2]
+
+    return diff_table
